485c2cd7a2c75e36346b1f389f43b80a8298bf58
IsThisUsernameFree <mdecpr@gmail.com>
2020-05-16 20:52:34 +0200

    Fixed minor stuff

--- client/build/docker-entrypoint.sh
+++ client/build/docker-entrypoint.sh
@@ -1,31 +1,11 @@
 #!/bin/bash
 
-function connect {
-	/usr/bin/ssh -R "0.0.0.0:$REMOTE_LISTEN:$LOCAL_HOST:$LOCAL_PORT" -o "ServerAliveInterval=5" -o "ServerAliveCountMax=2" -o "ConnectTimeout=10" -o "ExitOnForwardFailure=True" -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -i id_rsa.pem -p "$REMOTE_PORT" -N "root@$REMOTE_HOST" 2>/dev/null 1>&2 &
-	SSH_PID=$!
-	sleep 5
-	return $SSH_PID
-}
-
 function alive {
 	if kill -0 $1 2>/dev/null; then
 		return 0
 	fi
 	return 1
 }
-
-function monitor {
-	SSH_PID=$1
-	if alive $SSH_PID; then
-		echo "Connected to $REMOTE_HOST:$REMOTE_PORT. Forwarding port $REMOTE_LISTEN to $LOCAL_HOST:$LOCAL_PORT"
-	fi
-
-	while alive $SSH_PID; do
-		sleep 1
-	done
-}
-
-echo "Starting ssh client..."
 
 # Generate the user private key if not exist
 if [ ! -f "/root/id_rsa.pem" ]
@@ -37,34 +17,46 @@ then
 fi
 
 if [ -z "$REMOTE_HOST" ]; then
-	echo "Remote host argument missing"
+	echo "REMOTE_HOST argument missing"
 	exit 1
 fi
 
 if [ -z "$REMOTE_PORT" ]; then
-	echo "Remote port argument missing"
+	echo "REMOTE_PORT argument missing"
 	exit 1
 fi
 
 if [ -z "$REMOTE_LISTEN" ]; then
-	echo "Remote listen argument missing"
+	echo "REMOTE_LISTEN argument missing"
 	exit 1
 fi
 
 if [ -z "$LOCAL_HOST" ]; then
-	echo "Local host argument missing"
+	echo "LOCAL_HOST argument missing"
 	exit 1
 fi
 
 if [ -z "$LOCAL_PORT" ]; then
-	echo "Local port argument missing"
+	echo "LOCAL_PORT argument missing"
 	exit 1
 fi
 
+echo "Starting SSH client..."
 while true; do
-	connect
-	SSH_PID=$!
-	monitor $SSH_PID
+	# Init SSH pid to inexistent PID to make sure the while will run once.
+	SSH_PID=-1
+	while !(alive $SSH_PID); do
+		/usr/bin/ssh -R "0.0.0.0:$REMOTE_LISTEN:$LOCAL_HOST:$LOCAL_PORT" -o "ServerAliveInterval=5" -o "ServerAliveCountMax=2" -o "ConnectTimeout=10" -o "ExitOnForwardFailure=True" -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -i id_rsa.pem -p "$REMOTE_PORT" -N "root@$REMOTE_HOST" 2>/dev/null 1>&2 &
+		SSH_PID=$!
+		# Sleep 5 to let SSH crash if connect fails
+		sleep 5
+	done
+	# While is over, SSH is connected.
+	echo "$(date) : Connected to $REMOTE_HOST:$REMOTE_PORT (PID $SSH_PID). Forwarding port $REMOTE_LISTEN to $LOCAL_HOST:$LOCAL_PORT"
+
+	# Now wait for the PID to crash, notify it to the logs and loop.
+	while alive $SSH_PID; do
+		sleep 1
+	done
 	echo "$(date) : SSH crashed. Retrying..."
-	sleep 2
 done
