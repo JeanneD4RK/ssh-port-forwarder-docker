eadddc2e67c7e2479a9a41ff2810b6d2c88abc4b
IsThisUsernameFree <mdecpr@gmail.com>
2021-12-19 09:56:00 +0100

    Added support for multiple tunnels on client

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+function getTunnel {
+	IFS=":" read -a arr <<< "${1}"
+	HOST="${arr[0]}"
+	PORT="${arr[1]}"
+	LISTEN="${arr[2]}"
+	FORWARDHOST="${arr[3]}"
+	FORWARDPORT="${arr[4]}"
+}
+
+# If not args are passed to docker-entrypoint.sh, running normally.
 if [ "$#" -eq 0 ]; then
 	if [ -z "$MODE" ]; then
 		echo "MODE not specified. Running as server"
@@ -17,62 +28,52 @@ if [ "$#" -eq 0 ]; then
 		echo "OK: SSH service started"
 		/usr/sbin/sshd -D
 	elif [ "$MODE" == "client" ]; then
+		if [ -z "$CONF" ]; then
+		        echo "ERROR: CONF variable is not set."
+			exit
+		fi
+		if [ ! -r "$CONF" ]; then
+	        	echo "ERROR: File $CONF does not exist or is not readable."
+			exit
+		fi
+
 		echo "Starting forwarder as client mode"
-		# Check if the process is still running. Returns 0 if the process is alive, 1 if the process is dead (SSH crashed)
-		function alive {
-			if kill -0 "$1" 2>/dev/null; then
-				return 0
-			fi
-			return 1
-		}
+		
 		# Generate the user private key if not exist
 		if [ ! -f "/root/id_rsa.pem" ]; then
-			printf "%s\n\n%s\n\n%s" "#################################################################" "Generating a new keypair" "#################################################################"
+			printf "%s\n\n%s\n\n%s" "################################################################" "Generating a new keypair" "################################################################"
 			/usr/bin/ssh-keygen -q -N "" -f /root/id_rsa.pem
 			chmod 400 /root/id_rsa.pem
-			printf "\n\n\n%s\n\n%s\n\n%s\n\n" "The public key is :" "$(cat /root/id_rsa.pem.pub)" "#################################################################"
-		fi
-		if [ -z "$REMOTE_HOST" ]; then
-			echo "REMOTE_HOST argument missing"
-			exit 1
-		fi
-		if [ -z "$REMOTE_PORT" ]; then
-			echo "REMOTE_PORT argument missing"
-			exit 1
-		fi
-		if [ -z "$REMOTE_LISTEN" ]; then
-			echo "REMOTE_LISTEN argument missing"
-			exit 1
-		fi
-		if [ -z "$FORWARD_HOST" ]; then
-			echo "FORWARD_HOST argument missing"
-			exit 1
-		fi
-		if [ -z "$FORWARD_PORT" ]; then
-			echo "FORWARD_PORT argument missing"
-			exit 1
+			printf "\n\n\n%s\n\n%s\n\n%s\n\n" "The public key is :" "$(cat /root/id_rsa.pem.pub)" "################################################################"
 		fi
 	
-		echo "OK: SSH client started" 
-		echo "INFO: Trying to connect to $REMOTE_HOST:$REMOTE_PORT"
+		TUNNELS=()
+		COUNT=1
+		# Init tunnels array
+		while read LINE; do
+			getTunnel "$LINE"
+			if [ -z "$HOST" ] || 
+				[ -z "$PORT" ] || 
+				[ -z "$LISTEN" ] || 
+				[ -z "$FORWARDHOST" ] || 
+				[ -z "$FORWARDPORT" ] ||
+				! [ "$PORT" -eq "$PORT" ] 2>/dev/null ||
+				! [ "$LISTEN" -eq "$LISTEN" ] 2>/dev/null ||
+				! [ "$FORWARDPORT" -eq "$FORWARDPORT" ] 2>/dev/null ||
+				[ "$PORT" -le 0 ] 2>/dev/null ||
+				[ "$LISTEN" -le 0 ] 2>/dev/null ||
+				[ "$FORWARDPORT" -le 0 ] 2>/dev/null; then
+				echo "ERROR: Line $LINE is not a valid configuration."
+				exit
+			fi
+			./run_tunnel.sh "$COUNT" "$HOST" "$PORT" "$LISTEN" "$FORWARDHOST" "$FORWARDPORT" &
+			COUNT=$((COUNT+1))
+		done < "$CONF"
+
+		# Keep alive
 		while true; do
-			# Init SSH pid to inexistent PID to make sure the while will run once.
-			SSH_PID=-1
-			while ! (alive $SSH_PID); do
-				/usr/bin/ssh -R "0.0.0.0:$REMOTE_LISTEN:$FORWARD_HOST:$FORWARD_PORT" -o "ServerAliveInterval=5" -o "ServerAliveCountMax=2" -o "ConnectTimeout=10" -o "ExitOnForwardFailure=True" -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -i id_rsa.pem -p "$REMOTE_PORT" -N "root@$REMOTE_HOST" 2>/dev/null 1>&2 &
-				SSH_PID=$!
-				# Sleep 5 to let SSH crash if connect fails
-				sleep 5
-			done
-			# While is over, SSH is connected.
-			echo "$(date) : Connected to $REMOTE_HOST:$REMOTE_PORT (PID $SSH_PID). Forwarding TCP $REMOTE_HOST:$REMOTE_LISTEN to $FORWARD_HOST:$FORWARD_PORT"
-			# Infinite while to monitor PID
-			while alive $SSH_PID; do
-				sleep 1
-			done
-			# Notify crash in STDOUT and loop
-			echo "$(date) : SSH crashed. Retrying..."
-		done	
+			sleep 1
+		done
 	fi
 else
 	eval "$@"
---
+++ run_tunnel.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+function alive {
+        if kill -0 "$1" 2>/dev/null; then
+                return 0
+        fi
+        return 1
+}
+
+function startTunnel {
+	/usr/bin/ssh -R "0.0.0.0:$LISTEN:$FORWARDHOST:$FORWARDPORT" -o "ServerAliveInterval=5" -o "ServerAliveCountMax=2" -o "ConnectTimeout=10" -o "ExitOnForwardFailure=True" -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -i "id_rsa.pem" -p "$PORT" -N "root@$HOST" 1>&2 2>/dev/null &
+	SSH_PID=$!
+	# Sleep 5 to let SSH crash if connect fails
+	sleep 5
+}
+
+TUNNELID="$1"
+HOST="$2"
+PORT="$3"
+LISTEN="$4"
+FORWARDHOST="$5"
+FORWARDPORT="$6"
+
+SSH_PID=-1
+
+echo "[INFO] $(date) : SSH client #$TUNNELID started (connecting to $HOST:$PORT to forward port $LISTEN to $FORWARDHOST:$FORWARDPORT)" 
+
+startTunnel
+while true; do
+	# Try to connect until it's successful
+	while ! alive $SSH_PID; do
+		startTunnel
+	done
+
+	# While is over, SSH is connected.
+	echo "[OK] $(date) : Tunnel #$TUNNELID : Connected to $HOST:$PORT (PID $SSH_PID). Forwarding TCP :$LISTEN to $FORWARDHOST:$FORWARDPORT"
+
+	# Infinite while to monitor PID
+	while alive $SSH_PID; do
+		sleep 1
+	done
+
+	# At this point SSH tunnel is down
+	echo "[ERROR] $(date) : Tunnel #$TUNNELID crashed. Retrying..."
+done	
